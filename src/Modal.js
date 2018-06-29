3b20df497c93e1c226e8e0f827010bc495b3d78e
Shaun Yee <yeezy@Shauns-MacBook-Pro.local>
2018-06-28 20:51:46 -0700

    creates svg icon for close button on modal

--- src/Modal.js
+++ src/Modal.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Portal from './Portal';
+import Icon from './Utilities/Icon';
 
 export default class Modal extends Component {
   render() {
@@ -10,7 +11,9 @@ export default class Modal extends Component {
       {on && (
         <ModalWrapper>
           <ModalCard>
-            <CloseButton onClick={toggle}>Close</CloseButton>
+            <CloseButton onClick={toggle}>
+              <Icon name='close' />
+            </CloseButton>
             <div>{children}</div>
           </ModalCard>
           <Background onClick={toggle}/>
@@ -46,6 +49,9 @@ const CloseButton = styled.button`
   position: absolute;
   top: 0;
   right: 0;
+  border: none;
+  background: transparent;
+  padding: 10px;
 `;
 
 const Background = styled.div`
